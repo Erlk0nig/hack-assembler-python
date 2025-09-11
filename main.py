ffeee66711d6228bd2919604214d4d604c520e6a
Erlk0nig <191796679+Erlk0nig@users.noreply.github.com>
2025-09-11 17:44:50 +0100

    Add predefined symbols

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ class Parser:
             return ""
         return instruction.split(";")[1]
 
+
 class Code:
     def __init__(self,dest_field,comp_field,jump_field):
         self.dest_field = dest_field
@@ -158,6 +159,42 @@ class Code:
             case "JMP":
                 return "111"
 
+
+class SymbolTable:
+    def __init__(self,instruction):
+        self.instruction = instruction
+    
+    def initializer():
+        # Initialize the predefined symbols for the symbol table
+        predefined_symbols = {
+                               "R0": 0,
+                               "R1": 1,
+                               "R2": 2,
+                               "R3": 3, 
+                               "R4": 4, 
+                               "R5": 5, 
+                               "R6": 6, 
+                               "R7": 7, 
+                               "R8": 8,
+                               "R9": 9,
+                               "R10": 10,
+                               "R11": 11,
+                               "R12": 12,
+                               "R13": 13,
+                               "R14": 14,
+                               "R15": 15,
+                               "SP": 0,
+                               "LCL": 1,
+                               "ARG": 2,
+                               "THIS": 3,
+                               "THAT": 4,
+                               "SCREEN": 16384,
+                               "KBD": 24576
+                              }
+        return predefined_symbols
+    
+
+        
 # Select file to translate and store instructions
 path = Path(argv[1])
 parser = Parser(path)
